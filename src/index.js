f2b88a71bd1b1f7e688d84fc857da3e52b707c23
Nikita Mihadyuk <mihadyuk.nikita@gmail.com>
2020-02-24 14:13:21 +0300

    feat:add function towelSort at index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
 // You should implement your task here.
-
+/* function sortNumeric(a,b)
+{
+  if(a>b) return 1;
+  if(a==b) return 0;
+  if(a<b) return -1;
+}
+ */
 module.exports = function towelSort (matrix) {
-  return [];
+/*   let string=""; */
+  let array=[];
+  let counter=0;
+  if(matrix===undefined) return array;
+  for(let i=0;i<matrix.length;i++)
+  {
+    for(let j=0;j<matrix[i].length;j++)
+    {
+      if(i%2==0)
+      {
+         array[counter]=matrix[i][j];
+         counter++;
+      }
+      else{
+        array[counter]=matrix[i][(matrix[i].length-1)-j];
+        counter++;
+      }
+    }
+  }
+  return array;
 }
